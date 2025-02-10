d97fc2128d81fe772c62caa2923d963b7d6d2975
war-turtle <kartikyadav71199@gmail.com>
2025-02-10 15:55:43 +0530

    changes to update dotzsh and wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,16 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
-config.default_prog = { '/usr/bin/zsh' }
+config.color_scheme = "Tokyo Night"
+config.default_prog = { os.getenv("SHELL"), "-l" }
 config.enable_tab_bar = false
-config.window_background_opacity = 0.8
-config.font = wezterm.font 'JetBrains Mono'
+config.window_background_opacity = 0.9
+config.font = wezterm.font("JetBrains Mono", { weight = "Medium", italic = true })
+config.font_size = 15.0
 
 -- and finally, return the configuration to wezterm
 return config
